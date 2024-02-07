3d7bdb12660a4f1f3980ff2e4c53f2a191530388
Shahad <73951870+shahadsidek@users.noreply.github.com>
2024-02-07 13:38:33 +0300

    fixed JS Code

--- js/scripts.js
+++ js/scripts.js
@@ -2,30 +2,29 @@
 $(document).ready(() => {
 
     $('#loginMenu').hide()
-    
-$('#login').click(() => 
-{
-    $('#loginMenu').slideToggle()
-});   
- 
+
+    $('#login').click(() => {
+        $('#loginMenu').slideToggle()
+    });
+
 });
 
 
-  
+
 //OWL CAROUSEL PICTURE GALLERY
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:3
+        600: {
+            items: 3
         },
-        1000:{
-            items:5
+        1000: {
+            items: 5
         }
     }
 })
